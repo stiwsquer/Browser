9bb672fef710585ae1d006a92f38824fbdc4082f
Jakub Labryszewski <stiwsquer99@gmail.com>
2021-12-11 20:47:37 +0100

    Code refactoring

--- app.js
+++ app.js
@@ -7,13 +7,11 @@ button.addEventListener('click', () => generateRandomNumbers(1, 100));
 function generateRandomNumbers(min, max) {
   document.querySelectorAll('li').forEach((li) => li.remove());
   let randomNumbers = [];
-  for (let i = 0; i < 20; i++) {
+  for (let i = 0; i < 20; i++)
     randomNumbers.push(Math.floor(Math.random() * (max - min) + min));
-  }
+  randomNumbers.sort((a, b) => a - b);
   const oddNumbers = randomNumbers.filter((num) => num % 2);
   const evenNumbers = randomNumbers.filter((num) => !(num % 2));
-  oddNumbers.sort((a, b) => a - b);
-  evenNumbers.sort((a, b) => a - b);
   oddNumbers.forEach((num) => {
     let listItem = document.createElement('li');
     listItem.innerText = num;
